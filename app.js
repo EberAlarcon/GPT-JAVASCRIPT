507b11eb20865f145559995ad73d7877ddebb9ad
EberAlarcon <92482287+EberAlarcon@users.noreply.github.com>
2023-07-10 09:53:18 -0500

    MAIN

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log("Hola");
+
+const API_KEY_OPENIA = process.env.API_KEY
+
+const API_KEY = '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'
+
+//Funcion para llamar al metodo POST de la API 
+function getCompletion(){
+    fetch('https://api.openai.com/v1/completions',{
+        method: 'POST',
+        headers:{
+
+        }
+    })
+}
+
